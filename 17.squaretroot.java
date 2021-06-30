74bb0c3ae5eca32f7b56e1ccb7b1c5eaad8d79fa
deepakchinnadurai <74229102+deepakchinnadurai@users.noreply.github.com>
2021-06-30 17:51:02 +0530

    Update 17.squaretroot.java

--- 17.squaretroot.java
+++ 17.squaretroot.java
@@ -31,3 +31,26 @@ class Solution {
         return (int)high;
     }
 }
+
+
+
+
+
+//another method
+
+
+class Solution {
+    public int mySqrt(int x) {
+        if(x==0) return 0;
+        if(x==1) return 1;
+        int left=0,right=x,mid=(right-left)/2;
+        while(left<=right){
+            if(mid == (x/mid)) return mid;
+            else if(mid <(x/mid)) left = mid+1;
+            else if(mid >(x/mid)) right  = mid -1;
+            mid = left+(right-left)/2;
+            
+        }
+        return right;
+    }
+}
