f9d20e92f3ecc8e8448dd800afd9daddb64d0f1f
deepakchinnadurai <74229102+deepakchinnadurai@users.noreply.github.com>
2021-06-30 08:36:23 +0530

    Create 9.implement str().cpp
    it is one line code

---
+++ 9.implement str().cpp
@@ -0,0 +1,13 @@
+// Implement strStr().
+
+// Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+// Input: haystack = "hello", needle = "ll"
+// Output: 2
+
+
+class Solution {
+    public int strStr(String haystack, String needle) {
+       return haystack.indexOf(needle);
+    }
+}
